dfc48fb5daf4dfd5c964c14308713bc0a04aa22e
faisal01suryaa <faisal01suryaa@gmail.com>
2026-06-01 12:27:55 +0700

    menambahkan fungsi readgudang untuk membaca dan menampilkan daftar barang dari file gudang.txt

--- tokoelektronikgibranjaya.cpp
+++ tokoelektronikgibranjaya.cpp
@@ -30,3 +30,26 @@ public:
         }
     }
 };
+
+void readGudang() {
+    ifstream infile("gudang.txt");
+    cout << "\n=== Daftar Barang di Gudang (gudang.txt) ===" << endl;
+    
+    if (!infile.is_open()) {
+        cout << "File gudang.txt belum ada atau kosong." << endl;
+        return;
+    }
+    
+    string baris;
+    int no = 1;
+    bool isEmpty = true;
+    while (getline(infile, baris)) {
+        cout << "- " << baris << endl;
+        isEmpty = false;
+    }
+    
+    if (isEmpty) {
+        cout << "(Gudang Kosong)" << endl;
+    }
+    infile.close();
+}
